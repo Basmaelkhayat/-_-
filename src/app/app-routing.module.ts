c098563e2d50d2f114214813e9f5a04735f9b95a
BasmaElkhayat <Basmaelkhayat55@gmail.com>
2020-12-28 13:22:11 +0200

    fixing some dependency issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Router } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard } from './guards';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 // import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    //canActivate: [AuthGuard],
-    pathMatch: 'full',
-    redirectTo: 'home',
-  },
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('../app/pages/home/home.module').then((mod) => mod.HomeModule),
-  },
-  // { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: MainLayoutComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: '',
+                loadChildren: './layouts/main-layout/main-layout.module#MainLayoutModule',
+            },
+        ],
+    },
+    // { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      useHash: true,
-    }),
-  ],
+    imports: [
+        RouterModule.forRoot(routes, {
+            useHash: true,
+        }),
+    ],
 
-  exports: [RouterModule],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {
-  public constructor(private router: Router) {}
+    public constructor(private router: Router) {}
 }
