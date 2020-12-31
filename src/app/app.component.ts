ca1b3e71b08f1380a8e22cbd4a9a45255afbd3da
BasmaElkhayat <Basmaelkhayat55@gmail.com>
2020-12-31 16:42:39 +0200

    more responsive in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent{
   title = 'zeal-admin-panel';
+
+  constructor(private location: Location) {}
+
+  getMemberPadding() {
+    if (this.location.path() == '/members/member-page') {
+      return '1.5rem !important';
+    } else {
+      return '';
+    }
+  }
 }
