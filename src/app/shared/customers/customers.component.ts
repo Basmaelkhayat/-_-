15d5e1222994ae1c20adad0ba588a5a9ae3f52b4
ertomar <attallah.dev@gmail.com>
2021-01-27 14:06:26 +0200

    Updated members page table as it is in dashboard

--- src/app/shared/customers/customers.component.ts
+++ src/app/shared/customers/customers.component.ts
@@ -1,73 +1,151 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-customers',
-    templateUrl: './customers.component.html',
-    styleUrls: ['./customers.component.scss'],
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.scss'],
 })
 export class CustomersComponent implements OnInit {
-    searchText;
-    selectText :string ="Select property";
-    showFilter : boolean = true;
-    constructor() {}
+  searchText;
+  selectText: string = 'Select property';
+  showFilter: boolean = false;
+  selectCondition: string = '=';
+  showFilterTwo: boolean = false;
+  rangeValue = 'Select range';
+  inputValue: string = '';
+  showFilterPick: boolean = false;
+  addAnother: boolean = false;
+  body: any = {
+    filters: [],
+  };
+  dataBody: any = [];
+  customers: any = {
+    customerData: [],
+    searchValue: '',
+    filters: this.body.filters,
+    page_size: this.body,
+  };
+  filters: any = [];
+  column: string = '';
+  showDatePicker: boolean = false;
+  loaded: boolean = true;
+  condationName: string = '';
+  constructor() {}
 
-    ngOnInit() {
-    }
-    orders=[
-        { id: '1', name: 'Punch range' },
-        { id: '2', name: 'Punched reward' },
-        { id: '3', name: 'Date' },
-        { id: '4', name: 'Number of punches' },
-        { id: '5', name: 'Punch range' },
-        { id: '6', name: 'Punch range' },
-        { id: '7', name: 'Punch range' },
-    ]
-    
-    searchTextVal(value){
-        this.selectText=value;
-        this.showFilter = !this.showFilter
-        // this.memberService.storeMembers(this.body).subscribe();
-    }
-    changeStatus(){
-        this.showFilter = !this.showFilter
-        this.selectText ="Select property";
-    }
-    body = {
-      "sortBy": "created_at",
-      "sortDirection": "asc",
-      "filters": [
-          {
-              "column": "visits",
-              "condition": ">=",
-              "value": 1
-          },
-          {
-              "column": "spent",
-              "condition": "<",
-              "value": 9999
-          },
-          {
-              "column": "redeems",
-              "condition": ">",
-              "value": 1
-          },
-          {
-              "column": "signup_date",
-              "condition": ">=",
-              "value": "2020-01-01"
-          }
-          ,
-          {
-              "column": "first_punch",
-              "condition": ">=",
-              "value": "2020-01-01"
-          },
-          {
-              "column": "last_visit",
-              "condition": ">=",
-              "value": "2020-01-01"
-          }
-          ]
-      }
+  ngOnInit() {}
+  orders = [
+    { id: '1', name: 'Punch range' },
+    { id: '2', name: 'Punched reward' },
+    { id: '3', name: 'Date' },
+    { id: '4', name: 'Number of punches' },
+    { id: '5', name: 'Punch range' },
+    { id: '6', name: 'Punch range' },
+    { id: '7', name: 'Punch range' },
+  ];
 
+  probs = [
+    { column: 'visits', name: 'Visits' },
+    { column: 'spent', name: 'Spent' },
+    { column: 'redeems', name: 'Redeems' },
+    { column: 'signup_date', name: 'Signup Date' },
+    { column: 'first_punch', name: 'First Punch' },
+    { column: 'last_visit', name: 'Last Visit' },
+  ];
+  Pick = [
+    { condition: '=', name: 'Equals' },
+    { condition: '<', name: 'Lower than' },
+    { condition: '>', name: 'Bigger than' },
+    { condition: '<=', name: 'Equals & Lower than' },
+    { condition: '>=', name: 'Equals & Bigger than' },
+  ];
+  PickRange = [
+    { value: 10 },
+    { value: 20 },
+    { value: 30 },
+    { value: 40 },
+    { value: 50 },
+  ];
+  start = {
+    day: '',
+    month: '',
+    year: '',
+  };
+  onCustomerLoad(event) {
+    this.loaded = true;
+  }
+  dateSelected() {
+    this.rangeValue =
+      this.start.year + '-' + this.start.month + '-' + this.start.day;
+    this.showFilterPick = true;
+  }
+
+  searchTextVal(value, column) {
+    this.addAnother = false;
+    this.selectText = value;
+    this.condationName = value;
+    this.showFilter = true;
+    this.column = column;
+
+    if (
+      this.column == 'signup_date' ||
+      this.column == 'first_punch' ||
+      this.column == 'last_visit'
+    ) {
+      this.showDatePicker = true;
+    } else {
+      this.showDatePicker = false;
+    }
+  }
+  searchIconVal(value) {
+    this.selectCondition = value;
+    this.showFilterTwo = true;
+  }
+
+  searchPickVal(value) {
+    this.rangeValue = value;
+    this.showFilterPick = true;
+  }
+  deleteFilterItem(index) {
+    if (this.filters.length != 0) {
+      this.filters.splice(index, 1);
+      this.body.filters.splice(index, 1);
+    }
+  }
+
+  addAnotherFilter() {
+    this.selectText = 'Select property';
+    this.showFilter = false;
+    this.selectCondition = '=';
+    this.showFilterTwo = false;
+    this.rangeValue = 'Select range';
+    this.inputValue = '';
+    this.showFilterPick = false;
+    this.addAnother = false;
+  }
+  filterBody;
+  applyFilter() {
+    this.addAnother = true;
+    this.dataBody = {
+      filters: [
+        {
+          column: this.column,
+          condition: this.selectCondition,
+          value: this.rangeValue,
+        },
+      ],
+    };
+
+    this.filterBody = {
+      filters: [
+        {
+          column: this.condationName,
+          condition: this.selectCondition,
+          value: this.rangeValue,
+        },
+      ],
+    };
+
+    this.body.filters.push(...this.dataBody.filters);
+    this.filters.push(...this.filterBody.filters);
+  }
 }
--- src/app/shared/data-table/data-table.component.ts
+++ src/app/shared/data-table/data-table.component.ts
@@ -1,13 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-data-table',
-    templateUrl: './data-table.component.html',
-    styleUrls: ['./data-table.component.scss'],
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss'],
 })
 export class DataTableComponent implements OnInit {
+  dataTable: any;
+  names: any;
+  selectedAll: boolean = true;
+  loaded: boolean = true;
+  @Input() customers: any;
+  @Input() filters: any;
+  @Output() customersLoaded = new EventEmitter<boolean>(false);
 
-    constructor() {}
-    ngOnInit() {}
+  show: boolean = true;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.topCustomersData();
+  }
+  page_info;
+  ngOnChanges() {
+    this.dataTable = this.customers.customerData;
+    this.page_info = this.customers.page_size;
+  }
+  spinner: boolean = false;
+  custom_filters: any = [];
+  changePage(item) {}
+  topCustomersData() {}
+  CustomerPage(item) {
+    this.router.navigate(['/customers/customer-page', item.uuid]);
+  }
+
+  selectAll() {
+    if (this.selectedAll == true) {
+      for (var i = 0; i < this.dataTable.length; i++) {
+        this.dataTable[i].selected = this.selectedAll;
+      }
+      this.selectedAll = false;
+      return;
+    } else if (this.selectedAll == false) {
+      for (var i = 0; i < this.dataTable.length; i++) {
+        this.dataTable[i].selected = this.selectedAll;
+      }
+      this.selectedAll = true;
+    }
+    return;
+  }
 }
